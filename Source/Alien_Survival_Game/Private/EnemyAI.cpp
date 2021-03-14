b8cb6f9b9baba44653117a26d552f8bfff0a3b4e
Jacob <turnerj64@hotmail.co.uk>
2021-03-14 01:53:18 +0000

    Cleaned Up Enemy Modules and added model

---
+++ Source/Alien_Survival_Game/Private/EnemyAI.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyAI.h"
+#include "AIVision.h"
+
+// Sets default values
+AEnemyAI::AEnemyAI()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemyAI::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AEnemyAI::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AEnemyAI::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
